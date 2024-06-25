c54ec71bd7db24cead1d19304ae6f0b8a1520ecd
CosmicDweller <kvberry22@gmail.com>
2024-06-25 07:34:48 -0500

    update venv

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/kvberry/Code/Algo/streaming_app/venv"
+set -gx VIRTUAL_ENV "/Users/kvberry/Documents/GitHub/streaming_app/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
